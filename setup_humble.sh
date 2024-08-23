4ac84c817c5e042f1cbc9ccc70aff606c478e128
Nandagopan K <nandagopan.k01@gmail.com>
2024-08-23 15:04:38 +0530

    update Readme

--- setup_humble.sh
+++ setup_humble.sh
@@ -3,9 +3,9 @@
 # Set locale
 echo "Setting locale..."
 locale  # Check for UTF-8 support
-sudo apt update && sudo apt install -y locales
-sudo locale-gen en_US en_US.UTF-8
-sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+apt update && sudo apt install -y locales
+locale-gen en_US en_US.UTF-8
+update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 locale  # Verify settings
 
@@ -13,26 +13,28 @@ locale  # Verify settings
 echo "Setting up ROS 2 sources..."
 
 # Ensure Ubuntu Universe repository is enabled
-sudo apt install -y software-properties-common
-sudo add-apt-repository universe
+apt install -y software-properties-common
+add-apt-repository universe
 
 # Add the ROS 2 GPG key
-sudo apt update && sudo apt install -y curl
-sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+apt update 
+curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 # Add the repository to your sources list
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 # Install ROS 2 packages
 echo "Installing ROS 2 packages..."
-sudo apt update
-sudo apt upgrade -y
+apt update
+apt upgrade -y
 
 # Install Desktop version (recommended)
-sudo apt install -y ros-humble-desktop
+apt install -y ros-humble-desktop
 
 # Development tools
-sudo apt install -y ros-dev-tools
+apt install -y ros-dev-tools
+
+echo "source /opt/ros/humble/setup.bash" >> ~/.bashrc
 
 # Environment setup
 echo "Setting up environment..."
