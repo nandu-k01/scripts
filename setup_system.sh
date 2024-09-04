7494bda24cb48fc4d2c6e0b5eca1d602277db00d
Nandagopan K <nandagopan.k01@gmail.com>
2024-09-04 23:00:03 +0530

    changes to system_setup

--- setup_system.sh
+++ setup_system.sh
@@ -7,8 +7,8 @@ set -e
 echo "Setting locale..."
 locale  # Check for UTF-8 support
 sudo apt update && sudo apt install -y locales
-locale-gen en_US en_US.UTF-8
-update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 locale  # Verify settings
 
@@ -21,7 +21,7 @@ sudo add-apt-repository universe
 
 # Add the ROS 2 GPG key
 sudo apt update 
-curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 # Add the repository to your sources list
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
